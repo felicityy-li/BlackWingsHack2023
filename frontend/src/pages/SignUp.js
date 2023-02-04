69e97f4c8b6214de6aa759e8d3dc3909d2c6c00d
Meliora Ho <85920674+mellieho9@users.noreply.github.com>
2023-02-04 16:28:55 -0500

    changed the placement of signup

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -8,6 +8,7 @@ import {
   Select,
   MenuItem,
   Link,
+  Container,
   InputAdornment, IconButton
 } from "@mui/material";
 import VisibilityIcon from '@mui/icons-material/Visibility';
@@ -34,80 +35,83 @@ export const SignUp = ({ handleClose }) => {
   const options = ["Job Seeker", "Job Poster"];
 
   return (
-    <Stack spacing={5}>
-      <Stack sx={{ width: "100%" }} direction="column" alignItems="center">
-        <img style={{ height: 100, width: 100 }} src={icon_logo} alt="Logo" />
-      </Stack>
-      <Stack spacing={2} sx={{ mb: 5, position: "relative" }}>
-        <Typography sx={{ fontWeight: "bold" }} variant="h4">
-          Create an account
-        </Typography>
-        <form onSubmit={handleSubmit}>
-          <Stack spacing={3}>
-            <Stack spacing={1.5}>
-              <Stack direction={{ xs: "column", sm: "row" }} spacing={1.5} >
+    <Container sx={{ mt: 5 }} maxWidth="sm">
+      <Stack spacing={5}>
+        <Stack sx={{ width: "100%" }} direction="column" alignItems="center">
+          <img style={{ height: 100, width: 100 }} src={icon_logo} alt="Logo" />
+        </Stack>
+        <Stack spacing={2} sx={{ mb: 5, position: "relative" }}>
+          <Typography sx={{ fontWeight: "bold" }} variant="h4">
+            Create an account
+          </Typography>
+          <form onSubmit={handleSubmit}>
+            <Stack spacing={3}>
+              <Stack spacing={1.5}>
+                <Stack direction={{ xs: "column", sm: "row" }} spacing={1.5} >
+                  <TextField
+                    label="First Name"
+                    fullWidth
+                    variant="outlined"
+                    required
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                  />
+                  <TextField
+                    label="Last Name"
+                    fullWidth
+                    variant="outlined"
+                    required
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
+                </Stack>
+
                 <TextField
-                  label="First Name"
-                  fullWidth
+                  label="Email"
                   variant="outlined"
+                  type="email"
                   required
-                  value={firstName}
-                  onChange={(e) => setFirstName(e.target.value)}
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                 />
                 <TextField
-                  label="Last Name"
-                  fullWidth
+                  label="Password"
                   variant="outlined"
+                  type={showPassword ? "text" : "password"}
                   required
-                  value={lastName}
-                  onChange={(e) => setLastName(e.target.value)}
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  InputProps={{ endAdornment: (<InputAdornment><IconButton onClick={() => { setShowPassword(!showPassword) }}>{showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}</IconButton></InputAdornment>) }}
                 />
+                <TextField
+                  select
+                  label="User Type"
+                  defaultValue={options[0]}
+                  helperText="Please select your designation"
+                  onChange={(e) => setUserType(e.target.value)}
+                >
+                  {options.map((option) => (
+                    <MenuItem key={option} value={option}>
+                      {option}
+                    </MenuItem>
+                  ))}
+                </TextField>
               </Stack>
 
-              <TextField
-                label="Email"
-                variant="outlined"
-                type="email"
-                required
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <TextField
-                label="Password"
-                variant="outlined"
-                type={showPassword ? "text" : "password"}
-                required
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                InputProps={{ endAdornment: (<InputAdornment><IconButton onClick={() => { setShowPassword(!showPassword) }}>{showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}</IconButton></InputAdornment>) }}
-              />
-              <TextField
-                select
-                label="User Type"
-                defaultValue={options[0]}
-                helperText="Please select your designation"
-                onChange={(e) => setUserType(e.target.value)}
-              >
-                {options.map((option) => (
-                  <MenuItem key={option} value={option}>
-                    {option}
-                  </MenuItem>
-                ))}
-              </TextField>
+              <Stack direction="row" spacing={1}>
+                <Button size="large" fullWidth type="submit" variant="contained" color="secondary">
+                  Sign up
+                </Button>
+              </Stack>
+              <Stack direction="row" spacing={0.5} justifyContent="end">
+                <Typography variant="body2">Already have an account?</Typography>
+                <Link variant="body2" to="/">Sign in</Link>
+              </Stack>
             </Stack>
+          </form>
+        </Stack>
+      </Stack >
+    </Container>
 
-            <Stack direction="row" spacing={1}>
-              <Button size="large" fullWidth type="submit" variant="contained" color="secondary">
-                Sign up
-              </Button>
-            </Stack>
-            <Stack direction="row" spacing={0.5} justifyContent="end">
-              <Typography variant="body2">Already have an account?</Typography>
-              <Link variant="body2" to="/">Sign in</Link>
-            </Stack>
-          </Stack>
-        </form>
-      </Stack>
-    </Stack >
   );
 };
