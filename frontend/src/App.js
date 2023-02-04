598efbba120670fe92614456a4cb6c3944609ac1
Divya Ramaswamy <dsramasw@ncsu.edu>
2023-02-04 12:33:12 -0500

    add form skeleton

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { Form } from './components/Form';
+import { ThemeProvider, CssBaseline, createTheme, Button } from '@mui/material';
+
+const styles = {
+  formContainer: {
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: 'column',
+    alignItems: 'center',
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={styles.formContainer}>
+        <Form />
+      </div>
+    </>
   );
 }
 
-export default App;
+export default App; 
