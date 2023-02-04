091b4db7bfe6e12458b6cdd2d879c7b43196cfc9
Divya Ramaswamy <dsramasw@ncsu.edu>
2023-02-04 14:17:02 -0500

    add sign up page feature

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,16 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import SignIn from './pages/signin';
-
-
+import * as React from "react";
+import ReactDOM from "react-dom/client";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
+import { SignUpContainer } from "./pages/SignUpContainer";
 
 export default function App() {
   return (
     <Container>
-        <SignIn />
-      <Box>
-        
-      </Box>
+      <SignUpContainer />
+      <Box></Box>
     </Container>
   );
 }
---
+++ frontend/src/pages/SignUpContainer.js
@@ -0,0 +1,14 @@
+import { SignUp } from "./SignUp";
+import { Box } from "@mui/material";
+
+export const SignUpContainer = () => {
+  return (
+    <Box
+      sx={{
+        marginTop: 20,
+      }}
+    >
+      <SignUp />
+    </Box>
+  );
+};
