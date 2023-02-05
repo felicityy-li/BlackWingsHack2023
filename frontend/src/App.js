2b971ed1f0269a6f903f223b90057dabbaddee16
felicityy-li <88757077+felicityy-li@users.noreply.github.com>
2023-02-05 08:27:40 -0700

    fixed routing in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,8 @@ import { SignUpContainer } from "./pages/SignUpContainer";
 import { Routes, Route } from "react-router-dom";
 import SignIn from "./pages/Login";
 import Jobboard from "./pages/Jobboard";
+import JobForm from "./pages/JobForm";
+import JobFormSuccess from "./pages/JobFormSuccess";
 import UserProfilePage from "./pages/ProfilePage";
 import Profile from "./pages/Profile";
 
@@ -18,7 +20,12 @@ export default function App() {
         <Route path="/" element={<SignIn />} />
         <Route path="/signup" element={<SignUpContainer />} />
         <Route path="/jobboard" element={<Jobboard />}></Route>
-        <Route path="/profile-page" element={<Profile />}></Route>
+        <Route path="/profile" element={<Profile />}></Route>
+        <Route path="/create-posting" element={<JobForm />}></Route>
+        <Route
+          path="/create-posting-success"
+          element={<JobFormSuccess />}
+        ></Route>
       </Routes>
     </>
   );
