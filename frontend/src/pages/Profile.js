aba97b799dc5f58dbdc897ae29476509b31e6501
Meliora Ho <85920674+mellieho9@users.noreply.github.com>
2023-02-05 03:28:04 -0500

    implement profile

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,89 +1,190 @@
-import React from 'react'
-import { Box, Typography, Button, Grid, Stack} from "@mui/material";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Container, IconButton, Typography, Box, Stack, Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField, Avatar, Chip, List, ListItem, ListItemIcon, ListItemText } from "@mui/material";
+import Navbar from "../components/NavBar";
 import LocationOnIcon from '@mui/icons-material/LocationOn';
-import Navbar from '../components/NavBar';
+import CircleIcon from '@mui/icons-material/Circle';
+import EditIcon from '@mui/icons-material/Edit';
+export default function Profile() {
+    const [open, setOpen] = useState(false);
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
+    const saveUserInfo = (name) => {
+        setOpen(false);
+    };
+    const [userInfo, setUserInfo] = useState({
+        profilePicture:
+            "https://st3.depositphotos.com/7486768/17806/v/600/depositphotos_178065822-stock-illustration-profile-anonymous-face-icon-gray.jpg",
+        name: { firstName: "Divya", lastName: "Ramaswamy" },
+        location: { city: "Brooklyn", state: "NY" },
+        interests: ["Art", "Gym", "Coding"],
+        experience: ["Front Desk at Whiz Code Brooklyn", "Phone Assistant at NC State IT", "Student Ambassador at NC State"],
+        skills: ["Microsoft Office 365", "Notetaking", "Public Speaking", "Time Management", "Multitasking"]
+    });
 
-const Profile = () => {
-  return (
-    <Stack>
-        <Navbar isAuth={true} userType={"Job Seeker"} />
-        <Box sx={{ mt: 10 , mx: 10 }}>
-        
-        <Box sx={{ display: 'flex',
-                    flexDirection: 'row',
-                    justifyContent: 'center',
-            }} >
-            {/* User Info */}
-            <Box mr={15} sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center'
-            }}>
-                {/* User picture */}
-                <Box>
-                    <Box sx={{
-                        width: 270,
-                        height: 270,
-                        backgroundColor: '#C9AEED',
-                        borderRadius: 50,
-                    }}/>
-                </Box>
-                {/* User name, description and location */}
-                <Box mt={3} sx={{ display: 'flex', 
-                                    flexDirection: 'column', 
-                                    justifyContent: 'space-around', 
-                                    
-                }} >
-                    <Typography variant='h3' sx={{ fontWeight: 'bold'}}>Dese Elumaro</Typography>
-                    <Box mt={1} sx={{display: 'flex', alignItems: 'center'}}>
-                        <LocationOnIcon />
-                        <Typography ml={1} mt={1} variant='h6'>Durham, NC</Typography>
-                    </Box>
-                    <Typography variant='h6' mt={1}>Anime Lover, Xiao's mother</Typography>
-                </Box>
-                {/* User interests */}
-                <Grid container mt={3} rowSpacing={2} sx={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                }}>
-                    <Grid item>
-                        <Button variant='contained'>Content Creation</Button>
-                    </Grid>
+    const randomColor = () => {
+        let hex = Math.floor(Math.random() * 0xFFFFFF);
+        let color = "#" + hex.toString(16);
 
-                    <Grid item>
-                        <Button variant='contained'>Youtube</Button>
-                    </Grid>
-                    
-                    
-                </Grid>
-            </Box>
+        return color;
+    }
+    return (
+        <Container sx={{ mt: 5 }} maxWidth="100vw" >
+            <Navbar isAuth={true} userType={"Job Seeker"} />
+            <Stack sx={{ p: 5 }} fullWidth direction="row" spacing={20} justifyContent="center" alignItems="center">
+                <Stack spacing={3} sx={{ width: 200, pt: 10 }} direction="column" alignItems="center" justifyContent="center">
+                    <Stack direction="row" justifyContent="end">
+                        <Typography color="#ffffff">
+                            PLEASE WORK PLEASEEE
+                        </Typography>
+                        <IconButton sx={{ width: 5 }} onClick={handleClickOpen}>
+                            <EditIcon />
+                        </IconButton>
+                    </Stack>
+                    <Dialog
+                        open={open}
+                        onClose={handleClose}
+                        aria-labelledby="alert-dialog-title"
+                        aria-describedby="alert-dialog-description"
+                    >
+                        <DialogTitle id="alert-dialog-title">
+                            {"Update Profile"}
+                        </DialogTitle>
+                        <DialogContent>
+                            <Stack spacing={2} sx={{ width: 400 }}>
+                                <Typography sx={{ fontWeight: "bold" }}>City</Typography>
+                                <TextField
+                                    autoFocus
+                                    margin="dense"
+                                    id="city"
+                                    placeholder={userInfo.location.city}
+                                    type="text"
+                                    fullWidth
+                                    variant="outlined"
+                                />
+                                <Typography sx={{ fontWeight: "bold" }}>State</Typography>
+                                <TextField
+                                    autoFocus
+                                    margin="dense"
+                                    id="state"
+                                    placeholder={userInfo.location.state}
+                                    type="text"
+                                    fullWidth
+                                    variant="outlined"
+                                />
+                                <Typography sx={{ fontWeight: "bold" }}>Interests</Typography>
+                                <TextField
+                                    autoFocus
+                                    margin="dense"
+                                    id="interests"
+                                    placeholder={userInfo.interests}
+                                    type="text"
+                                    fullWidth
+                                    variant="outlined"
+                                />
+                            </Stack>
 
-
-            {/* Experience and Skills */}
-            <Box sx={{ display: 'flex',
-                        flexDirection: 'column',
-                        justifyContent: 'space-evenly',
-            }} >
-                {/* Experience */}
-                <Box>
-                    <Typography variant='h3' mb={2} sx={{ fontWeight: 'bold'}}>Experience</Typography>
-                    <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, omnis rerum repellat eum perferendis corrupti possimus nobis similique veniam aliquid ab incidunt alias ratione? Mollitia commodi maiores saepe cum in! <br /> <br />
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt molestias accusamus soluta sapiente cumque vero omnis error eligendi ipsa nisi ipsam qui quia alias repudiandae, maiores, officia ex illo ducimus?
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={handleClose}>Cancel</Button>
+                            <Button onClick={handleClose} autoFocus>
+                                Save
+                            </Button>
+                        </DialogActions>
+                    </Dialog>
+                    <Avatar src="/broken-image.jpg" sx={{ width: 150, height: 150 }} />
+                    <Typography align="center" variant="h4" sx={{ fontWeight: "bold" }}>
+                        {userInfo.name.firstName} {userInfo.name.lastName}
                     </Typography>
-                </Box>
-                {/* Skills */}
-                <Box>
-                    <Typography variant='h3' mb={2} sx={{ fontWeight: 'bold'}}>Skills</Typography>
-                    <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus tempora numquam dolores error nesciunt blanditiis, vitae architecto ea quam at possimus rerum dicta natus ab fuga eum fugit voluptate. Expedita?</Typography>
-                </Box>
-            </Box>
-        </Box>
-        
-    </Box>
-    </Stack>
-  )
-}
+                    <Stack direction="row" spacing={0.5} alignItems="center" justifyContent="flex-start" >
+                        <LocationOnIcon style={{ color: "#F15C5C" }} />
+                        <Typography variant="h6">{userInfo.location.city}, {userInfo.location.state}</Typography>
+                    </Stack>
+                    <Stack direction="row" spacing={0.5} justifyContent="flex-start">
+                        {userInfo.interests.map((data) => {
+                            return (<Chip style={{ backgroundColor: randomColor(), color: "#ffffff" }} label={data} />)
+                        })}
+                    </Stack>
+                </Stack>
+                <Stack direction="column" spacing={5} alignItems="flex-start" justifyContent="flex-start">
 
-export default Profile
+                    <Stack spacing={2}>
+                        <Stack direction="row" spacing={3} alignItems="center">
+                            <Typography sx={{ fontWeight: "bold" }} variant="h4">
+                                Experience
+                            </Typography>
+                            <IconButton sx={{ width: 5 }}>
+                                <EditIcon onClick={handleClickOpen} />
+                            </IconButton>
+                            <Dialog>
+                                <DialogTitle id="alert-dialog-title">
+                                    {"Update Experience"}
+                                </DialogTitle>
+                                <DialogContent>
+                                    <Stack spacing={2} sx={{ width: 400 }}>
+                                        <Typography sx={{ fontWeight: "bold" }}>Experience</Typography>
+                                        <TextField
+                                            autoFocus
+                                            margin="dense"
+                                            id="experience"
+                                            placeholder={userInfo.experience}
+                                            type="text"
+                                            fullWidth
+                                            multiline
+                                            rows={7}
+                                            variant="outlined"
+                                        />
+                                    </Stack>
+
+                                </DialogContent>
+                                <DialogActions>
+                                    <Button onClick={handleClose}>Cancel</Button>
+                                    <Button onClick={handleClose} autoFocus>
+                                        Save
+                                    </Button>
+                                </DialogActions>
+                            </Dialog>
+                        </Stack>
+
+                        <List sx={{ spacing: 0.5 }}>
+                            {userInfo.experience.map((data) => {
+                                return (
+                                    <ListItem>
+                                        <ListItemIcon>
+                                            <CircleIcon sx={{ width: 5 }} />
+                                        </ListItemIcon>
+                                        <ListItemText
+                                            primary={data}
+
+                                        />
+                                    </ListItem>
+                                )
+                            })}
+                        </List>
+                    </Stack>
+                    <Stack spacing={2}>
+                        <Stack direction="row" spacing={3} alignItems="center">
+                            <Typography sx={{ fontWeight: "bold" }} variant="h4">
+                                Skills
+                            </Typography>
+                            <IconButton sx={{ width: 5 }}>
+                                <EditIcon />
+                            </IconButton>
+                        </Stack>
+                        <Stack direction="row" spacing={0.5} justifyContent="flex-start">
+                            {userInfo.skills.map((data) => {
+                                return (<Chip label={data} />)
+                            })}
+                        </Stack>
+                    </Stack>
+
+                </Stack>
+            </Stack>
+
+
+        </Container>
+    );
+}
